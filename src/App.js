14afcad68ad467255bc4755de1653d2c01a2493f
rio-damanik <168444216+rio-damanik@users.noreply.github.com>
2024-08-12 01:59:29 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import RegistrationForm from "./RegistrationForm";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Registration Form</h1>
+      <RegistrationForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/RegistrationForm.jsx
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+
+const RegistrationForm = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [errors, setErrors] = useState({});
+  const [successMessage, setSuccessMessage] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const validate = () => {
+    const errors = {};
+
+    if (!formData.firstName) {
+      errors.firstName = "First name is required";
+    }
+
+    if (!formData.lastName) {
+      errors.lastName = "Last name is required";
+    }
+
+    if (!formData.email) {
+      errors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      errors.email = "Email is invalid";
+    }
+
+    if (!formData.password) {
+      errors.password = "Password is required";
+    } else if (formData.password.length < 6) {
+      errors.password = "Password must be at least 6 characters";
+    }
+
+    if (!formData.confirmPassword) {
+      errors.confirmPassword = "Confirm password is required";
+    } else if (formData.confirmPassword !== formData.password) {
+      errors.confirmPassword = "Passwords do not match";
+    }
+
+    return errors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = validate();
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      setSuccessMessage("");
+    } else {
+      setErrors({});
+      console.log(formData);
+      setSuccessMessage("Registration successful!");
+      alert(JSON.stringify(formData, null, 2));
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {Object.keys(errors).length > 0 && (
+        <div className="error-message">
+          <p>Please fix the errors below:</p>
+        </div>
+      )}
+
+      {successMessage && (
+        <div className="success-message">
+          <p>{successMessage}</p>
+        </div>
+      )}
+
+      <div>
+        <label>First Name</label>
+        <input type="text" name="firstName" value={formData.firstName} onChange={handleChange} />
+        {errors.firstName && <p>{errors.firstName}</p>}
+      </div>
+      <div>
+        <label>Last Name</label>
+        <input type="text" name="lastName" value={formData.lastName} onChange={handleChange} />
+        {errors.lastName && <p>{errors.lastName}</p>}
+      </div>
+      <div>
+        <label>Email</label>
+        <input type="email" name="email" value={formData.email} onChange={handleChange} />
+        {errors.email && <p>{errors.email}</p>}
+      </div>
+      <div>
+        <label>Password</label>
+        <input type="password" name="password" value={formData.password} onChange={handleChange} />
+        {errors.password && <p>{errors.password}</p>}
+      </div>
+      <div>
+        <label>Confirm Password</label>
+        <input type="password" name="confirmPassword" value={formData.confirmPassword} onChange={handleChange} />
+        {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+      </div>
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default RegistrationForm;
